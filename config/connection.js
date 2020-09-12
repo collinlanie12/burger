9c07d04639770d662143808aa5d5c5420d0f8b77
Collin Lanier <collinlanier12@gmail.com>
2020-09-11 23:53:41 -0400

    adding jawsdb

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,18 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+  });
+};
+
 // Make connection.
 connection.connect(function (err) {
   if (err) {
@@ -20,3 +32,4 @@ connection.connect(function (err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+connection.connect();
