8ef64b878c73fd45e985dc060f23c236f15983da
Collin Lanier <collinlanier12@gmail.com>
2020-09-12 14:00:36 -0400

    H10 error

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require("express");
 
-var PORT = process.env.PORT || '36247';
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 app.set("port", PORT);
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("./public"));
+app.use(express.static("public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +19,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
